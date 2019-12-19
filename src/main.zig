b0ef230cd214f75f3a3d13241b63d809d1bf28c3
Darin Minamoto <darinm223@gmail.com>
2019-12-19 06:04:06 -0800

    Repl parses input with mpc and prints ast

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,38 @@ const std = @import("std");
 const c = @import("c.zig");
 
 pub fn repl() std.os.ReadError!void {
+    const Number = c.mpc_new("number");
+    const Operator = c.mpc_new("operator");
+    const Expr = c.mpc_new("expr");
+    const Lispy = c.mpc_new("lispy");
+
+    _ = c.mpca_lang(c.MPCA_LANG_DEFAULT,
+        \\ number   : /-?[0-9]+/ ;
+        \\ operator : '+' | '-' | '*' | '/' ;
+        \\ expr     : <number> | '(' <operator> <expr>+ ')' ;
+        \\ lispy    : /^/ <operator> <expr>+ /$/ ;
+    , Number, Operator, Expr, Lispy);
+    defer c.mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
     var input: [2048]u8 = undefined;
     const stdin = std.io.getStdIn();
     while (true) {
         std.debug.warn("lispy> ", .{});
         const bytes_read = try stdin.read(&input);
-        std.debug.warn("No you're a {}", .{input[0..bytes_read]});
+        // Add null character for C string support.
+        if (bytes_read > 0) {
+            input[bytes_read - 1] = 0;
+        }
+        var r: c.mpc_result_t = undefined;
+        if (c.mpc_parse("<stdin>", input[0..bytes_read].ptr, Lispy, &r) != 0) {
+            var ptr = @ptrCast(*c.mpc_ast_t, @alignCast(@alignOf(c.mpc_ast_t), r.output.?));
+            c.mpc_ast_print(ptr);
+            c.mpc_ast_delete(ptr);
+        } else {
+            var ptr = @ptrCast([*c]c.mpc_err_t, @field(r, "error").?);
+            c.mpc_err_print(ptr);
+            c.mpc_err_delete(ptr);
+        }
     }
 }
 
